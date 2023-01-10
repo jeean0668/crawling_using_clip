4f6a696f6c36d5263be04be70d9de6f2d604275f
jeean0668 <gijeeankyung@naver.com>
2023-01-10 00:54:15 +0000

    consume2_receive_and_crawl

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from json import loads
 bootstrap_servers = ["host.docker.internal:9092"]
 
 # 카프카 토픽
-str_topic_name    = 'Topic2'
+str_topic_name    = 'Topic1'
 
 # 카프카 소비자 group1 생성
 ### 
 str_group_name = 'group1'
-consumer = KafkaConsumer(str_topic_name, bootstrap_servers=bootstrap_servers,
+consumer2 = KafkaConsumer(str_topic_name, bootstrap_servers=bootstrap_servers,
                          auto_offset_reset='earliest', # 가장 처음부터 소비
                          enable_auto_commit=True,
                          group_id=str_group_name,
@@ -24,12 +24,17 @@ consumer = KafkaConsumer(str_topic_name, bootstrap_servers=bootstrap_servers,
 
 if __name__ == "__main__":
     
-    for message in consumer:
+    custom_crawler = crawler.GoogleImageCrawler()
+    index = 0 
+    for message in consumer2:
+        print(message.value)
         image_nums = message.value['image_nums']
         keywords = message.value['keywords']
-        print(message)
-        custom_crawler = crawler.GoogleImageCrawler(
-            maximum_image_count = message.value['image_nums'])
+        custom_crawler.maximum_image_count = message.value['image_nums']
         custom_crawler.search(message.value['keywords'])
+        print(f"{index+1}/{image_nums} processing finished")
+        index += 1
+        
+    print("finished")
     
     
